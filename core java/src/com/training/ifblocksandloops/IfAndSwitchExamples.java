a34f5ca23a6b4256e8b58f743506827157237ab3
venkataranga <dinesh.csit@gmail.com>
2015-06-04 19:06:34 -0500

    operators and if and switch blocks

--- core java/src/com/training/ifblocksandloops/IfAndSwitchExamples.java
+++ core java/src/com/training/ifblocksandloops/IfAndSwitchExamples.java
@@ -1,41 +1,11 @@
-package com.training.operators;
+package com.training.ifblocksandloops;
 
 import com.training.classes.Car;
 
-public class Operators {
-
+public class IfAndSwitchExamples {
+	
 	public static void main(String[] args) {
-		
-		int i = 10;
-		int j;
-		j = i;
-		System.out.println(j);
-		
-		Car c = new Car();
-		c.$color="blue";
-		
-		Car c1;
-		c1=c;
-		c1.$color="red";
-		
-		System.out.println(c.$color);
-		
-		i++;
-		System.out.println(i);
-		
-		boolean b = true;
-		System.out.println(!b);
-		System.out.println(11%2);
-		
-		System.out.println(i==11);
-		String s1 = "hello";
-		String s2 = "hello";
-		
-		System.out.println(s1==s2);
-		
-		c1.$color="yellow";
-		c1._model="honda";
-		c1.type1="Accord";
+		int i=10;
 		
 		Car c2 = new Car();
 		c2.$color="yellow";
@@ -43,9 +13,6 @@ public class Operators {
 		c2.type1="Accord";
 		c2.make="honda";
 		
-		System.out.println("c1==c2: "+(c1==c2));
-		c2 = c1;
-		System.out.println("c1==c2: "+(c1==c2));
 		
 		if(i==11 && (c2.$color.equals("blue") && c2.make.equals("honda"))){
 			System.out.println("i value is 11 and c2 car color is blue");
@@ -65,15 +32,15 @@ public class Operators {
 			
 		}else if (i==11){
 			System.out.println("else if is executed, i value is 11");
-		}else if(c1==null) 
-			System.out.println("c1 is null");
+		}else if(c2==null) 
+			System.out.println("c2 is null");
 	
 		if(c2._model!=null) 
 		System.out.println("honda color is not blue or make is not honda");
 		
 		System.out.println("if blocks are completed ");
 	
-		i=10;
+		
 		switch(i+10){
 		
 			case 10:
